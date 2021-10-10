fadbda7e331fb4ced5eb5b963aac79c1c38afc09
srkdongare <srkdongare@gmail.com>
2021-10-10 18:50:54 +0530

    minor changes

--- product-api/data/products.go
+++ product-api/data/products.go
@@ -14,7 +14,7 @@ type Product struct {
 	ID          int     `json:"id"`
 	Name        string  `json:"name"  validate:"required"`
 	Description string  `json:"description"`
-	Price       float32 `json:"price"`
+	Price       float32 `json:"price" validate:"required,gte=0,lte=100"`
 	SKU         string  `json:"sku"`
 	CreatedOn   string  `json:"-"`
 	UpdatedOn   string  `json:"-"`
